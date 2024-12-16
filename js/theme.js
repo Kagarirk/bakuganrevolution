426c3fa96ab0f0d8455b33b2c495805d8a3df849
Kagarirk <128372914+Kagarirk@users.noreply.github.com>
2024-12-16 17:53:00 +0200

    +

--- js/theme.js
+++ js/theme.js
@@ -6,3 +6,5 @@ const themeLink = document.getElementById('theme-style');
 themeLink.href = `./css/${CURRENT_THEME}/style.css`
 const themeLinkMobile = document.getElementById('theme-style-mobile');
 themeLinkMobile.href = `./css/${CURRENT_THEME}/style_mobile.css`
+const themePreloader = document.getElementById('theme-preloader');
+themePreloader.href = `./css/${CURRENT_THEME}/preloader.css`
