5e54bc9c6ea78d0f2468177c0fc421992846025a
Kagarirk <128372914+Kagarirk@users.noreply.github.com>
2024-12-17 21:52:10 +0200

    +

--- js/theme.js
+++ js/theme.js
@@ -8,3 +8,10 @@ const themeLinkMobile = document.getElementById('theme-style-mobile');
 themeLinkMobile.href = `./css/${CURRENT_THEME}/style_mobile.css`
 const themePreloader = document.getElementById('theme-preloader');
 themePreloader.href = `./css/${CURRENT_THEME}/preloader.css`
+
+const themeTg = document.getElementById('tg');
+themeTg.src = `./css/${CURRENT_THEME}/img/telegram_icon.svg`
+const themeFb = document.getElementById('fb');
+themeFb.src = `./css/${CURRENT_THEME}/img/facebook_icon.svg`
+const themeGh = document.getElementById('gh');
+themeGh.src = `./css/${CURRENT_THEME}/img/github_icon.svg`
