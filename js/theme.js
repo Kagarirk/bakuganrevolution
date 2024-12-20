e5b6fcc2b5dfa0f57b8e65c1c950984085d99314
Kagarirk <128372914+Kagarirk@users.noreply.github.com>
2024-12-20 15:43:19 +0200

    +

--- js/theme.js
+++ js/theme.js
@@ -1,5 +1,5 @@
 // Вкажіть тут назву поточної теми
-const CURRENT_THEME = 'new_year'; // Змінюйте на 'default', 'new_year' тощо
+const CURRENT_THEME = 'default'; // Змінюйте на 'default', 'new_year' тощо
 
 // Автоматично підключаємо CSS-файл для вибраної теми
 const themeLink = document.getElementById('theme-style');
@@ -9,6 +9,8 @@ themeLinkMobile.href = `./css/${CURRENT_THEME}/style_mobile.css`
 const themePreloader = document.getElementById('theme-preloader');
 themePreloader.href = `./css/${CURRENT_THEME}/preloader.css`
 
+const themeHero = document.getElementById('hero');
+themeHero.src = `./css/${CURRENT_THEME}/img/hero.jpg`
 const themeTg = document.getElementById('tg');
 themeTg.src = `./css/${CURRENT_THEME}/img/telegram_icon.svg`
 const themeFb = document.getElementById('fb');
